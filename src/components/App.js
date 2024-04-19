a87b2a4af915bcffedef30e763cb8b62f1ba0679
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-19 08:28:00 +0000

    fetchingData

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  function DataFetch() {
+    axios.get("https://dummyjson.com/products").then((response) => {
+      setData(response.data);
+      setIsLoading(false);
+    }).catch((error) => {
+      setError(error.message);
+      setIsLoading(false);
+    })
+  }
+
+  useEffect(() => {
+    DataFetch();
+    return () => {
+
+    }
+  }, []);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        {isLoading ? (<p>Loading...</p>) : error ? (<p>Error: {error}</p>) : (<pre>Data Fetched from API<h1></h1>{JSON.stringify.data}</pre>)}
     </div>
   )
 }
