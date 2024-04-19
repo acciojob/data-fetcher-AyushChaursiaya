8f86f1e7f9a3a79f594529e27a37b698bff588f7
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-19 12:20:34 +0000

    fetchAPI

--- src/components/App.js
+++ src/components/App.js
@@ -20,42 +20,23 @@ const App = () => {
   }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://dummyjson.com/products');
-        setData(response.data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-
-    // Clean up function to cancel fetch request (if needed)
+    DataFetch();
     return () => {
-      // Cancel fetch request or cleanup (if any)
-    };
+
+    }
   }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
-  if (!data) {
-    return <p>No data found</p>;
-  }
 
   return (
     <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {/* Do not remove the main div */}
+      {isLoading ?
+        (<p>Loading...</p>) :
+        error ?
+          (<p>Error: {error}</p>) :
+          data ?
+            (<pre><h1>Data Fetched from API</h1>{JSON.stringify(data, null, 2)}</pre>) : (<p>No data found</p>)}
     </div>
-  );
+  )
 }
 
 export default App
